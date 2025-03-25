5ed3c6233502ba15d385c829ef684e3b92d335d6
hachi <andreicosta3110@gmail.com>
2025-03-24 21:47:44 -0300

    Add files via upload

---
+++ andreideverL1.js
@@ -0,0 +1,22 @@
+let matriz1 = [
+    [25, 21, 30],
+    [33, 32, 28],
+    [16, 22, 24]
+  ];
+
+  let matriz2 = [
+    [4, 10, 64],
+    [8, 6, 12],
+    [14, 16, 18]
+  ];
+  let matriz3 = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ];
+
+  for (let i = 0; i < matriz1.length;) 
+    for (let j = 0; j < matriz1[j].length;){
+matriz3 = matriz1[i][j] + matriz2[i][j]
+    }
+return matriz3
