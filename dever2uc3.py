cb97da135ce96ed1ac8c29d8860b28ffba82c63e
hachi <andreicosta3110@gmail.com>
2025-05-05 19:43:06 -0300

    Add files via upload

---
+++ dever2uc3.py
@@ -0,0 +1,42 @@
+lista_teste = [10000, 20000, 30000, 40000]
+print(lista_teste[1]) 
+print(lista_teste[1:3])
+print(lista_teste[-1])  
+print(lista_teste[-2:])  
+print(lista_teste[:2])   
+print(lista_teste[::2])  
+print(lista_teste[::-1])    
+print(lista_teste[1:4:2])  
+print(lista_teste[1:]) 
+print(lista_teste[:3])  
+print(lista_teste[-3:-1])          
+print(len(lista_teste))  
+print(lista_teste.count(20000))
+
+dados = {"madeira": 10000, "manteiga": 20000, "frango": 30000, "costeleta": 40000}
+print(dados.get("madeira", "manteiga"))
+
+print(list(dados.values()))
+
+colaborador = {
+  "madeira": 10000, 
+  "manteiga": 20000, 
+  "frango": 30000, 
+  "costeleta": 40000
+}
+
+novo_colaborador = colaborador.copy()
+print(novo_colaborador)
+
+conjunto = {1, 2, 3, 4, 4}
+print(conjunto)  
+
+conjunto.discard(2)
+print(conjunto)
+
+for ir in (dados.values():
+           
+if > 1000
+
+lista_teste.append()
+print(lista_teste)
